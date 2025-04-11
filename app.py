9bf43f89ab5ce68cc2c56bdabf2bf2754dea442a
Bruno-SouzaR <srosabruno@gmail.com>
2025-04-10 22:42:51 -0300

    Deploy config

--- app.py
+++ app.py
@@ -3,12 +3,21 @@ from src.models import db, ma
 from config.settings import DATABASE_URI
 from src.routes.routes import bp as routes_bp
 from flask_cors import CORS
+from src.utils.constants import ENVIRONMENT
+from config.settings import environment, front_end_url
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-CORS(app)
+origins_map = {
+    ENVIRONMENT.LOCAL.value: ['*'],
+    ENVIRONMENT.PRODUCTION.value: [front_end_url]
+}
+
+allwed_origin = origins_map.get(environment, origins_map[ ENVIRONMENT.PRODUCTION.VALUE ])
+
+CORS(app, origins = allwed_origin)
 
 db.init_app(app)
 ma.init_app(app)
---
+++ src/utils/constants.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class ENVIRONMENT(Enum):
+    LOCAL = "local"
+    PRODUCTION = "production"

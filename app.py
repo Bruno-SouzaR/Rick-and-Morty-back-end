79dd16e349b3e85d8de69f2dd281bef16f0cd4db
Bruno-SouzaR <srosabruno@gmail.com>
2025-04-11 22:01:57 -0300

    Deploy config

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 origins_map = {
     ENVIRONMENT.LOCAL.value: ['*'],
-    ENVIRONMENT.PRODUCTION.value: ["*"],
+    ENVIRONMENT.PRODUCTION.value: [front_end_url]
 }
 
 allowed_origins = origins_map.get(environment, origins_map[ENVIRONMENT.PRODUCTION.value])
@@ -21,11 +21,14 @@ print(f"Front-end URL: {front_end_url}")
 print(f"Allowed origins: {allowed_origins}")
 
 CORS(app,
-     origins=allowed_origins,
-     supports_credentials=True,
-     allow_headers=["Content-Type", "Authorization", "Accept"],
-     methods=["GET", "OPTIONS"], 
-     expose_headers=["Content-Type"])
+     resources={r"/*": {
+         "origins": allowed_origins,
+         "supports_credentials": True,
+         "allow_headers": ["Content-Type", "Authorization", "Accept"],
+         "methods": ["GET", "OPTIONS"],
+         "expose_headers": ["Content-Type"],
+         "vary_header": True
+     }})
 
 db.init_app(app)
 ma.init_app(app)
